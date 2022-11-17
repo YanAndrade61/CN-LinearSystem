415c2d46287045f4e0a8db1d2785a5272b95328c
yan <yanccomp@gmail.com>
2022-11-17 15:15:07 -0300

    MMQ Algorithm

---
+++ mmq.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int mmq(float x[],float y[],int n){
+
+    
+    
+}
+
+int main(){
+    
+    int x[] = {1,2,3,4.5,6,7.5,9,10,11};
+    int y[] = {2,25,70,80,220,250,440,500,560}; 
+
+
+    return 0;
+}
